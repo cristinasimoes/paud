14c3261894e4b294b6c8c5939d77c6d1a1a00a1b
Cristina Pinto Simões <cristinapinto.simoes@gmail.com>
2021-07-24 19:54:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 var navBar= document.getElementById('navigation');
 var clipboard= document.querySelector('.clipboard');
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      navBar.style.backgroundColor = "white";
-      navBar.style.transition = "linear 1s";
-
-
-  } else {
-   
-    navBar.style.backgroundColor = "initial";
-
-  }
-
-}
-// function scrollFunction() {
-//   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-//       navBar.style.backgroundColor = "rgba(255, 255, 255,1);";
-
-//   } else {
-   
-//     navBar.style.backgroundColor = "rgba(255, 255, 255,1);";
-//   }
-// }
    // copy to clipboard
 function myFunction(id) {
     clipboard.style.display="block";
@@ -75,6 +51,8 @@ gsap.to("nav", {
 });
 
 
+
+//scroll animation background color change
 const [red, green, blue] = [255, 255, 255]
     const section1 = document.querySelector('.body-bg')
 
@@ -83,72 +61,31 @@ const [red, green, blue] = [255, 255, 255]
       const y = 1 + (window.scrollY || window.pageYOffset) / 106
       const [r, g, b] = [red/y, (green/y)-1, (blue/y)-2].map(Math.round)
             section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-            console.log(section1.style.backgroundColor);
+         
 
 
      
     })
 
-    
-
-// gsap.to(".background-text1 h2:last-child", {
-//   scrollTrigger: {
-//     trigger: ".section1",
-//     start: "bottom 80%",
-//   end: "bottom 30%",
-//     markers:false,
-//   },
-//   x: "-420px",
-//   duration:1, 
-// });
 
 
+//scroll animation navbar text color change
 
 
-
-// gsap.to("body", {
-//   scrollTrigger: {
-//     trigger: ".section1",
-//     toggleActions: "restart none reverse reverse",                     
-//     start: "bottom 70%",
-//     end: "bottom 90%",
-//     // scroller: "#scrollWrapper",
-//     markers:true,
-//     scrub:1,
-//   },
-//   backgroundColor: 'var(--black)',
-//   duration: 2,
-// });
+const [red2, green2, blue2] = [34, 33, 32]
+const section2 = document.querySelectorAll('.nav-color');
 
 
+window.addEventListener('scroll', () => {
+  const y = 1 + (window.scrollY || window.pageYOffset) / 106
+  const [r, g, b] = [red2*y, (green2*y)-1, (blue2*y)-2].map(Math.round)
+  var i;
+  for(i=0; i<4; i++){
+    section2[i].style.color = `rgb(${r}, ${g}, ${b})`
+  }
+     
 
 
-// animation portrait slide down
-// gsap.to(".portrait", {
-//   scrollTrigger: {
-//     trigger: ".text-section",
-//     toggleActions: "restart none reverse reverse",                     
-//     start: "top 80%",
-//     end: "top 30%",
-//     scroller: "#scrollWrapper",
-//     markers:false,
-//     scrub:1,
-//   },
-//   y:'50vh',
-//   x:'0',
-//   duration: 2,
-// });
+ 
+})
 
-// gsap.to("#homeSection header", {
-//   scrollTrigger: {
-//     trigger: ".nav-bottom",
-//     toggleActions: "restart none reverse reverse",
-//     start: "top 83%",
-//     end: "top 85%",
-//     scroller: "#scrollWrapper",
-//     markers:false,
-//   },
-//   y: "-420px",
-//   ease: "Power1.easeInOut",
-//   duration: 1.5,
-// });
