9c8370b969dc26b5edc1aa75377e9cce43c5c08b
Cristina Pinto Simões <cristinapinto.simoes@gmail.com>
2021-08-01 18:25:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-var navBar= document.getElementById('navigation');
+// copy to clipboard function
 var clipboard= document.querySelector('.clipboard');
-
-
-// copy to clipboard
 function myFunction(id) {
     clipboard.style.display="block";
     var text = document.querySelector(id).innerText;
@@ -12,10 +9,9 @@ function myFunction(id) {
     elem.select();
     document.execCommand("copy");
     document.body.removeChild(elem);
-  }
+}
 
-// gsap animation
-// animation portrait opacity
+// gsap animation - delay on the background text
 gsap.to(".background-text1", {
   scrollTrigger: {
     trigger: ".section1",
@@ -25,22 +21,28 @@ gsap.to(".background-text1", {
   delay: 1,
   duration:1, 
 });
+// gsap animation - delay on the span title 
+gsap.to(".background-text1 span", {
+  scrollTrigger: {
+    trigger: ".section1",
+    markers:false,
+  },
+  opacity:1,
+  delay: 2,
+  duration:3, 
+});
+// gsap animation - delay on the navigation bar
+gsap.to("nav", {
+  scrollTrigger: {
+    trigger: ".section1",
+    markers:false,
+  },
+  opacity:1,
+  delay: 2,
+  duration:3, 
+});
 
-// gsap.to(".background-text1", {
-//   scrollTrigger: {
-//     trigger: ".background-text2",
-//     start:'top 90%',
-//     end: 'bottom 70%',
-//     markers:true,
-//     toggleAction:"play restart restart restart",
-
-//   },
-//   y: -400,
-//   // opacity:1,
-//   // delay: 1,
-//   duration:1, 
-// });
-
+// gsap animation - bg text slide up when scroll
 gsap.to(".background-text1", {
   scrollTrigger: {
     trigger: ".background-text2 p",
@@ -54,25 +56,6 @@ gsap.to(".background-text1", {
   duration: 1.5,
 });
 
-gsap.to(".background-text1 span", {
-  scrollTrigger: {
-    trigger: ".section1",
-    markers:false,
-  },
-  opacity:1,
-  delay: 2,
-  duration:3, 
-});
-gsap.to("nav", {
-  scrollTrigger: {
-    trigger: ".section1",
-    markers:false,
-  },
-  opacity:1,
-  delay: 2,
-  duration:3, 
-});
-
 //scroll animation background color change
 const [red, green, blue] = [255, 255, 255]
     const section1 = document.querySelector('.body-bg')
@@ -81,13 +64,10 @@ const [red, green, blue] = [255, 255, 255]
       const [r, g, b] = [red/y, (green/y)-1, (blue/y)-2].map(Math.round)
             section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`     
     })
-
-
-
+    
 //scroll animation navbar text color change
 const [red2, green2, blue2] = [34, 33, 32]
 const section2 = document.querySelectorAll('.nav-color');
-
 window.addEventListener('scroll', () => {
   const y = 1 + (window.scrollY || window.pageYOffset) / 106
   const [r, g, b] = [red2*y, (green2*y)-1, (blue2*y)-2].map(Math.round)
